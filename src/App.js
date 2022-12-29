589b12d6ff7b6ddd6691f940e4a7081e72d194aa
Sofia Tejada <79775413+sofiatejada@users.noreply.github.com>
2022-12-29 13:23:32 -0800

    just some practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import Todo from './Todo';
 
-function App() {
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete',
+};
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.banana)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map((singleTodo) => {
+        if (singleTodo.id === action.payload.id) {
+          return { ...singleTodo, complete: !singleTodo.complete };
+        }
+        return singleTodo;
+      });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((singleTodo) => singleTodo.id !== action.payload.id);
+    default:
+      return todos;
+  }
+};
+
+const newTodo = (banana) => {
+  return { id: Date.now(), banana: banana, complete: false };
+};
+
+export default function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [banana, setBanana] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { banana: banana } });
+    setBanana('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={banana}
+          onChange={(e) => setBanana(e.target.value)}
+        />
+      </form>
+      {todos.map((singleTodo) => {
+        return (
+          <Todo key={singleTodo.id} todo={singleTodo} dispatch={dispatch} />
+        );
+      })}
+    </>
   );
 }
-
-export default App;
